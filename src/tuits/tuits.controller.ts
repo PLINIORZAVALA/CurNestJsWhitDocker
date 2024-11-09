b0968a24629036c2732735b0684b8027de236042
PLINIORZAVALA <134644204+PLINIORZAVALA@users.noreply.github.com>
2024-11-09 14:36:34 -0500

    Parametros para hacer la llamada mediante un ID desde la peticion HTTP

--- src/tuits/tuits.controller.ts
+++ src/tuits/tuits.controller.ts
@@ -1,9 +1,14 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Param } from '@nestjs/common';
 
 @Controller('tuits')
 export class TuitsController {//resive peticiones http:...
     @Get()
-    getTuits() {
+    getTuits(): string {
         return 'Hello from Tuitter';
+    }
+
+    @Get(':id') //tuit1
+    getTuit(@Param('id') id: string): string{//Decorador que se obtiene desde el cliente
+        return `Your tuit id is ${id}`;
     }
 }
